83173561498d0d84e4f475ba4adbbba59613e6d9
Juan Jose Mazo <juangamer2455@gamil.com>
2025-08-28 00:11:20 -0600

    supapito

---
+++ ejemplospoo/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+var cuenta = new CuantasBancarias();
+cuenta.Depositar(1300600.22);
+cuenta.Retirar(600000);
+
+public class CuantasBancarias
+{
+    public double Saldo = 0;
+
+    public void Depositar(double monto)
+    {
+        Saldo += monto;
+        Console.WriteLine($"la cantidad de dinero en la cuenta actualmente es:{Saldo}");
+    }
+    
+    public void Retirar(double monto)
+    {
+        Saldo -= monto;
+        Console.WriteLine($"la cantidad de dinero en la cuenta actualmente es:{Saldo}"); 
+    }
+}
